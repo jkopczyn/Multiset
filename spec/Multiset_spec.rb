0f478e87a00bf84276b441080b48e26deba48e29
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-06 13:43:54 -0800

    some tests to fill in later

--- spec/Multiset_spec.rb
+++ spec/Multiset_spec.rb
@@ -5,7 +5,63 @@ describe Multiset do
     expect(Multiset::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'can create an empty multiset' do
+      expect(Multiset.new).to_not raise_error
+  end
+
+  it 'can create a multiset from an array' do
+      fail
+  end
+
+  it 'can create a multiset from a set' do
+      fail
+  end
+
+  it 'can create a multiset from another multiset' do
+      fail
+  end
+
+  it 'tests equality correctly' do
+      fail
+  end
+
+  it 'can add elements' do
+      fail
+  end
+
+  it 'can remove elements' do
+      fail
+  end
+
+  it 'cleans elements with zero instances' do
+      fail
+  end
+
+  it 'can erase all instances of an element' do
+      fail
+  end
+
+  it 'can iterate over all elements in the set' do
+      fail
+  end
+
+  it 'can iterate through all element, number pairs' do
+      fail
+  end
+
+  it 'can give its own size' do
+      fail
+  end
+
+  it 'can clear itself' do
+      fail
+  end
+
+  it 'can check whether it is empty' do
+      fail
+  end
+
+  it 'can convert to an array' do
+      fail
   end
 end
