dca0b9121ad447b6d26284f66744e5f9d9319ce7
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-06 00:44:21 -0800

    move to proper location

--- lib/Multiset.rb
+++ lib/Multiset.rb
@@ -1,5 +1,32 @@
 require "Multiset/version"
 
 module Multiset
-  # Your code goes here...
+    class Multiset
+        def initialize(iterable=[])
+            @hash = Hash.new(0)
+            iterable.each { |i| self.add(i) }
+        end
+
+        def add(item, count=1)
+            @hash[item] += count
+            @hash[item] == 0 ? @hash.delete(item) : @hash[item]
+        end
+
+        def remove(item, count=1)
+            @hash[item] -= count
+            @hash[item] == 0 ? @hash.delete(item) : @hash[item]
+        end
+
+        def dump(item)
+            @hash.delete(item)
+        end
+
+        def items
+            @hash.keys
+        end
+
+        def counts
+            @hash.entries
+        end
+    end
 end
